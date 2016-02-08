9238877af76421ba3788510e50fd3561aab20ede
Sergei Panfilov <s.panfilov@maximatelecom.ru>
2016-02-08 17:24:52 +0300

    Added app.manifest creation package

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp         = require('gulp'),
     sass         = require('gulp-sass'),
     wrap         = require('gulp-wrap'),
     autoprefixer = require('gulp-autoprefixer'),
-    browserSync  = require('browser-sync').create();
+    browserSync  = require('browser-sync').create(),
+    manifest     = require('gulp-manifest');
 
 var paths = {
     assets      : 'assets/branding',
@@ -49,4 +50,15 @@ gulp.task('prod', function () {
     return gulp.src(paths.layout)
         .pipe(wrap({src: paths.production}))
         .pipe(gulp.dest(paths.output));
+});
+gulp.task('manifest', function(){
+  gulp.src(['assets/branding/*'], { base: './' })
+    .pipe(manifest({
+      timestamp: true,
+      cache: ['/brandings/_cacheAssets/application.css','/brandings/_cacheAssets/application.js'],
+      network: ['*'],
+      filename: 'app.manifest',
+      exclude: 'app.manifest'
+    }))
+    .pipe(gulp.dest('./'));
 });
